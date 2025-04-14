3dacc75d7d23af396c71525dbb038a94d9116bb3
ZaiD4804 <ZaiD4804@sduhsd.lan>
2025-04-14 08:42:11 -0700

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return (
+        <>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+        <hr></hr>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/Stuff.jsx
@@ -0,0 +1,18 @@
+
+
+function Stuff({name = "Employee Man", title = "Worker"}){
+    return (
+        
+        <div className='myCard'>
+            <img className="cardPic" src="https://placehold.co/100x100"></img>
+            <h3>{name}</h3>
+            <p>{title}</p>
+        </div>
+        
+    );
+}
+
+
+
+
+export default Stuff;
