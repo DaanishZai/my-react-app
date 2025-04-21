bc12b3e5133235638b79c6a93a5e997b12d18eae
DaanishZai <156246978+DaanishZai@users.noreply.github.com>
2025-04-21 09:35:18 -0700

    Updates

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Stuff from './Stuff.jsx';
+import MyDemo from './MyDemo.jsx';
+import Clock from './Clock.jsx'
+
+function App(){
+  return(
+    <>
+    <Header/>
+    <MyDemo/>
+    <Clock/>
+    <Stuff name="Bob Smith" title="CEO"/>
+    <Stuff name="Sue Jones" title="CFO"/>
+    
+    <Stuff/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAosSFkqU4vnfMnlitDYQKgjmGOs84q-FI",
+  authDomain: "react-firebase-app-2b2f1.firebaseapp.com",
+  projectId: "react-firebase-app-2b2f1",
+  storageBucket: "react-firebase-app-2b2f1.firebasestorage.app",
+  messagingSenderId: "283411754843",
+  appId: "1:283411754843:web:d067dcab3318a58678b47e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
